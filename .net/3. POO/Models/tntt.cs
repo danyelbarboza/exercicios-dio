6e8a82d845930220ebb9298d733e411ae60e03b0
Danyel Barboza <dhgbarboza@gmail.com>
2025-07-09 15:52:03 -0300

    Exercicios bancos de dados

---
+++ .net/3. POO/Models/tntt.cs
@@ -0,0 +1,14 @@
+        
+        int limit = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < limit; i++) 
+        {
+            string[] line = Console.ReadLine().Split(" ");
+            double X = double.Parse();
+            double Y = double.Parse();
+            if (   ) {
+                
+
+            } else {
+                
+            }
+        }
